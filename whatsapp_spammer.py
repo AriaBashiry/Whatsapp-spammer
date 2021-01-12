fd073418d28e51d6e7e4751fea0268ce991603f3
Aria124 <45628883+Aria124@users.noreply.github.com>
2021-01-12 17:11:36 +0330

    Update and rename whatsappbot.py to whatsapp_spammer.py
    editing whatsappspammer

--- whatsapp_spammer.py
+++ whatsapp_spammer.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome("D:\chromedriver.exe")
+driver = webdriver.Chrome("chromedriver.exe")
 
 driver.get("https://web.whatsapp.com/")
 
@@ -21,5 +21,4 @@ for name in names:
     for i in range(count):
         msgbox.send_keys(msg)
         button = driver.find_element_by_class_name('_1U1xa')
-        #button = driver.find_element_by_id("sendbtn")
         button.click()
