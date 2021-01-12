89cb936abed81e599f7f9308ab0e3e6977bfbdd7
Aria124 <45628883+Aria124@users.noreply.github.com>
2021-01-12 17:05:34 +0330

    Add files via upload
    whatsapp spammer commit

---
+++ whatsappbot.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome("D:\chromedriver.exe")
+
+driver.get("https://web.whatsapp.com/")
+
+names = input('Enter names: ')
+s = names.split(',')
+names = list(s)
+
+msg = input('Enter message: ')
+count = int(input('How many times? '))
+
+input('If you are sure, press enter...')
+
+for name in names:
+    user = driver.find_element_by_xpath("//span[@title = '{}']".format(name))
+    user.click()
+    msgbox = driver.find_element_by_class_name('_3FRCZ')
+
+    for i in range(count):
+        msgbox.send_keys(msg)
+        button = driver.find_element_by_class_name('_1U1xa')
+        #button = driver.find_element_by_id("sendbtn")
+        button.click()
